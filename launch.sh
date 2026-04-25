bd1328485346fcf6da56fa957a3203ff6d53ea9f
niklasblock <niklas.block.business@gmail.com>
2026-04-25 18:42:48 +0200

    fix app launcher with logging and full uvicorn path

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /Users/niklasblock/src/home/budgettracker
-uvicorn app.main:app --host 127.0.0.1 --port 8000 &
-sleep 2
+/Library/Frameworks/Python.framework/Versions/3.13/bin/uvicorn app.main:app --host 127.0.0.1 --port 8000 > /tmp/budgettracker.log 2>&1 &
+sleep 4
 open -a "Google Chrome" http://127.0.0.1:8000/static/index.html
